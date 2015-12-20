0312a101fe393ed0cbd668f94d3cadc43f51d07c
alex hawley <asimo124@yahoo.com>
2015-12-20 12:28:21 -0600

    First Commit

---
+++ SQLTableExport/Form1.Designer.vb
@@ -0,0 +1,222 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtHost = New System.Windows.Forms.TextBox()
+        Me.txtUsername = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtPassword = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtDatabase = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.txtConnString = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.txtTable = New System.Windows.Forms.TextBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.btnExport = New System.Windows.Forms.Button()
+        Me.lblMessage = New System.Windows.Forms.Label()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(16, 51)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(29, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Host"
+        '
+        'txtHost
+        '
+        Me.txtHost.Location = New System.Drawing.Point(19, 67)
+        Me.txtHost.Name = "txtHost"
+        Me.txtHost.Size = New System.Drawing.Size(263, 20)
+        Me.txtHost.TabIndex = 1
+        '
+        'txtUsername
+        '
+        Me.txtUsername.Location = New System.Drawing.Point(19, 117)
+        Me.txtUsername.Name = "txtUsername"
+        Me.txtUsername.Size = New System.Drawing.Size(263, 20)
+        Me.txtUsername.TabIndex = 3
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(16, 101)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(55, 13)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Username"
+        '
+        'txtPassword
+        '
+        Me.txtPassword.Location = New System.Drawing.Point(19, 168)
+        Me.txtPassword.Name = "txtPassword"
+        Me.txtPassword.Size = New System.Drawing.Size(263, 20)
+        Me.txtPassword.TabIndex = 5
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(16, 152)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(53, 13)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Password"
+        '
+        'txtDatabase
+        '
+        Me.txtDatabase.Location = New System.Drawing.Point(19, 219)
+        Me.txtDatabase.Name = "txtDatabase"
+        Me.txtDatabase.Size = New System.Drawing.Size(263, 20)
+        Me.txtDatabase.TabIndex = 7
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(16, 203)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(53, 13)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = "Database"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(16, 260)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(23, 13)
+        Me.Label5.TabIndex = 8
+        Me.Label5.Text = "OR"
+        '
+        'txtConnString
+        '
+        Me.txtConnString.Location = New System.Drawing.Point(19, 306)
+        Me.txtConnString.Name = "txtConnString"
+        Me.txtConnString.Size = New System.Drawing.Size(263, 20)
+        Me.txtConnString.TabIndex = 10
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(16, 289)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(91, 13)
+        Me.Label6.TabIndex = 9
+        Me.Label6.Text = "Connection String"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.lblMessage)
+        Me.GroupBox1.Controls.Add(Me.txtDatabase)
+        Me.GroupBox1.Controls.Add(Me.txtHost)
+        Me.GroupBox1.Controls.Add(Me.txtUsername)
+        Me.GroupBox1.Controls.Add(Me.txtConnString)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.Label6)
+        Me.GroupBox1.Controls.Add(Me.Label5)
+        Me.GroupBox1.Controls.Add(Me.txtPassword)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 21)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(299, 348)
+        Me.GroupBox1.TabIndex = 12
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Connect to DB"
+        '
+        'txtTable
+        '
+        Me.txtTable.Location = New System.Drawing.Point(31, 411)
+        Me.txtTable.Name = "txtTable"
+        Me.txtTable.Size = New System.Drawing.Size(263, 20)
+        Me.txtTable.TabIndex = 12
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(28, 395)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(79, 13)
+        Me.Label7.TabIndex = 11
+        Me.Label7.Text = "Table to Export"
+        '
+        'btnExport
+        '
+        Me.btnExport.Location = New System.Drawing.Point(219, 457)
+        Me.btnExport.Name = "btnExport"
+        Me.btnExport.Size = New System.Drawing.Size(75, 23)
+        Me.btnExport.TabIndex = 13
+        Me.btnExport.Text = "Export"
+        Me.btnExport.UseVisualStyleBackColor = True
+        '
+        'lblMessage
+        '
+        Me.lblMessage.AutoSize = True
+        Me.lblMessage.ForeColor = System.Drawing.Color.Red
+        Me.lblMessage.Location = New System.Drawing.Point(16, 27)
+        Me.lblMessage.Name = "lblMessage"
+        Me.lblMessage.Size = New System.Drawing.Size(0, 13)
+        Me.lblMessage.TabIndex = 11
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(325, 508)
+        Me.Controls.Add(Me.btnExport)
+        Me.Controls.Add(Me.txtTable)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents txtHost As System.Windows.Forms.TextBox
+    Friend WithEvents txtUsername As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents txtPassword As System.Windows.Forms.TextBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents txtDatabase As System.Windows.Forms.TextBox
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents txtConnString As System.Windows.Forms.TextBox
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents txtTable As System.Windows.Forms.TextBox
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents btnExport As System.Windows.Forms.Button
+    Friend WithEvents lblMessage As System.Windows.Forms.Label
+
+End Class
---
+++ SQLTableExport/Form1.vb
@@ -0,0 +1,54 @@
+﻿Imports System.Data.SqlClient
+
+Public Class Form1
+
+    Private myConn As SqlConnection
+    Private myCmd As SqlCommand
+    Private myReader As SqlDataReader
+    Private results As String
+
+    Private Sub Form1_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub btnExport_Click(sender As System.Object, e As System.EventArgs) Handles btnExport.Click
+
+        Dim Host As String = Me.txtHost.Text
+        Dim Username As String = Me.txtUsername.Text
+        Dim Password As String = Me.txtPassword.Text
+        Dim Database As String = Me.txtDatabase.Text
+        Dim ConnStr As String = Me.txtConnString.Text
+        Dim Table As String = Me.txtTable.Text
+
+        If ConnStr = "" And Host <> "" And Username <> "" And Password <> "" And Database <> "" Then
+            myConn = New SqlConnection("Server=" & Host & ";Database=" & Database & ";User Id=" & Username & ";Password=" & Password & ";")
+        ElseIf ConnStr <> "" Then
+            myConn = New SqlConnection(ConnStr)
+        End If
+
+        If Table <> "" Then
+            Dim file As System.IO.StreamWriter
+            file = My.Computer.FileSystem.OpenTextFileWriter("c:\temp\" & Table & ".txt", False)
+            myCmd = myConn.CreateCommand
+            myCmd.CommandText = "SELECT * from " & Table
+            myConn.Open()
+            myReader = myCmd.ExecuteReader()
+            Dim datastr = ""
+            Do While myReader.Read()
+                datastr = ""
+                For i As Integer = 0 To myReader.FieldCount - 1
+                    If i = 0 Then
+                        datastr = myReader.Item(i)
+                    Else
+                        datastr = datastr & "~" & myReader.GetString(i)
+                    End If
+                Next
+                datastr = datastr & "}"
+                file.Write(datastr)
+            Loop
+            file.Close()
+
+            lblMessage.Text = "Export Complete."
+        End If
+    End Sub
+End Class
