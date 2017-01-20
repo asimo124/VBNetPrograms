3172b87a5c23eefdd5b4f22c3e3d872b1ee4fa3a
Alex Hawley <asimo1242yahoo.com>
2017-01-19 21:40:35 -0600

    GrepSearch

---
+++ GrepSearch/WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.cboPaths = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtSearchTerm = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.cboFileTypes = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.dgvSearchResults = new System.Windows.Forms.DataGridView();
+            this.pathDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.lineDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.openNotePadDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.searchResultBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.cboJustFiles = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSearchResults)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.searchResultBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cboPaths
+            // 
+            this.cboPaths.FormattingEnabled = true;
+            this.cboPaths.Location = new System.Drawing.Point(12, 35);
+            this.cboPaths.Name = "cboPaths";
+            this.cboPaths.Size = new System.Drawing.Size(390, 21);
+            this.cboPaths.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(124, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Select Path to Search";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 91);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(390, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(408, 88);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Add";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtSearchTerm
+            // 
+            this.txtSearchTerm.Location = new System.Drawing.Point(522, 91);
+            this.txtSearchTerm.Name = "txtSearchTerm";
+            this.txtSearchTerm.Size = new System.Drawing.Size(347, 20);
+            this.txtSearchTerm.TabIndex = 1;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(519, 73);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(133, 15);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Regex String to Search";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(875, 89);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 3;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // cboFileTypes
+            // 
+            this.cboFileTypes.FormattingEnabled = true;
+            this.cboFileTypes.Location = new System.Drawing.Point(522, 35);
+            this.cboFileTypes.Name = "cboFileTypes";
+            this.cboFileTypes.Size = new System.Drawing.Size(121, 21);
+            this.cboFileTypes.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(105, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "OR Add New Path";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(519, 17);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(56, 15);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "File Type";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // dgvSearchResults
+            // 
+            this.dgvSearchResults.AutoGenerateColumns = false;
+            this.dgvSearchResults.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvSearchResults.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.pathDataGridViewTextBoxColumn,
+            this.lineDataGridViewTextBoxColumn,
+            this.openNotePadDataGridViewTextBoxColumn});
+            this.dgvSearchResults.DataSource = this.searchResultBindingSource;
+            this.dgvSearchResults.Location = new System.Drawing.Point(21, 130);
+            this.dgvSearchResults.Name = "dgvSearchResults";
+            this.dgvSearchResults.Size = new System.Drawing.Size(1754, 627);
+            this.dgvSearchResults.TabIndex = 8;
+            this.dgvSearchResults.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvSearchResults_CellContentClick);
+            // 
+            // pathDataGridViewTextBoxColumn
+            // 
+            this.pathDataGridViewTextBoxColumn.DataPropertyName = "Path";
+            this.pathDataGridViewTextBoxColumn.HeaderText = "Path";
+            this.pathDataGridViewTextBoxColumn.Name = "pathDataGridViewTextBoxColumn";
+            this.pathDataGridViewTextBoxColumn.Width = 700;
+            // 
+            // lineDataGridViewTextBoxColumn
+            // 
+            this.lineDataGridViewTextBoxColumn.DataPropertyName = "Line";
+            this.lineDataGridViewTextBoxColumn.HeaderText = "Line";
+            this.lineDataGridViewTextBoxColumn.Name = "lineDataGridViewTextBoxColumn";
+            this.lineDataGridViewTextBoxColumn.Width = 700;
+            // 
+            // openNotePadDataGridViewTextBoxColumn
+            // 
+            this.openNotePadDataGridViewTextBoxColumn.DataPropertyName = "OpenNotePad";
+            this.openNotePadDataGridViewTextBoxColumn.HeaderText = "OpenNotePad";
+            this.openNotePadDataGridViewTextBoxColumn.Name = "openNotePadDataGridViewTextBoxColumn";
+            this.openNotePadDataGridViewTextBoxColumn.Width = 200;
+            // 
+            // searchResultBindingSource
+            // 
+            this.searchResultBindingSource.DataSource = typeof(WindowsFormsApplication1.SearchResult);
+            // 
+            // cboJustFiles
+            // 
+            this.cboJustFiles.AutoSize = true;
+            this.cboJustFiles.Location = new System.Drawing.Point(680, 36);
+            this.cboJustFiles.Name = "cboJustFiles";
+            this.cboJustFiles.Size = new System.Drawing.Size(114, 19);
+            this.cboJustFiles.TabIndex = 9;
+            this.cboJustFiles.Text = "Just File Names";
+            this.cboJustFiles.UseVisualStyleBackColor = true;
+            this.cboJustFiles.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1787, 769);
+            this.Controls.Add(this.cboJustFiles);
+            this.Controls.Add(this.dgvSearchResults);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtSearchTerm);
+            this.Controls.Add(this.cboFileTypes);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cboPaths);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSearchResults)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.searchResultBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cboPaths;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtSearchTerm;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.ComboBox cboFileTypes;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DataGridView dgvSearchResults;
+        private System.Windows.Forms.BindingSource searchResultBindingSource;
+        private System.Windows.Forms.DataGridViewTextBoxColumn pathDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn lineDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn openNotePadDataGridViewTextBoxColumn;
+        private System.Windows.Forms.CheckBox cboJustFiles;
+    }
+}
+
---
+++ GrepSearch/WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+   
+
+    public partial class Form1 : Form
+    {
+        public List<string> paths = new List<string>();
+        public List<string> pathNames = new List<string>();
+        public SearchResult[] SearchResults = new SearchResult[0];
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            loadDropdownPaths();
+            loadDropdownFileTypes();
+            this.SearchResults = new SearchResult[0];
+            dgvSearchResults.DataSource = this.SearchResults;
+        }
+
+        private void loadDropdownPaths()
+        {
+            int counter = 1;
+            string line;
+
+            StreamReader file =
+               new StreamReader("C:\\temp\\vb_grep_dropdown_paths.txt");
+            while ((line = file.ReadLine()) != null)
+            {
+                string[] pathArr;
+                pathArr = line.Split('\t');
+
+                this.paths.Add(pathArr[0]);      // Path
+                this.pathNames.Add(pathArr[1]);  // Path Name
+
+                cboPaths.Items.Add(pathArr[1]);
+                counter++;
+            }
+            file.Close();
+
+            cboPaths.SelectedIndex = 0;
+        }
+
+        private void loadDropdownFileTypes()
+        {
+            cboFileTypes.Items.Add("ALL");
+            cboFileTypes.Items.Add("PHP");
+            cboFileTypes.Items.Add("JS");
+            cboFileTypes.Items.Add("HTM");
+            cboFileTypes.Items.Add("CSS");
+            cboFileTypes.Items.Add("SQL");
+            cboFileTypes.Items.Add("PY");
+            cboFileTypes.Items.Add("JAVA");
+
+            cboFileTypes.SelectedIndex = 0;
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (txtSearchTerm.Text.Trim() != "")
+            {
+
+                String cur_path = this.paths[cboPaths.SelectedIndex];
+
+                String cur_file_type = cboFileTypes.SelectedItem.ToString();
+                if (cur_file_type == "ALL")
+                {
+                    cur_file_type = "*";
+                }
+
+                this.SearchResults = new SearchResult[0];
+                dgvSearchResults.DataSource = this.SearchResults;
+                List<string> files = Directory.GetFiles(cur_path, "*." + cur_file_type, SearchOption.AllDirectories).ToList();
+
+                if (cboJustFiles.Checked == false)
+                {
+                    foreach (String getFile in files)
+                    {
+                        int counter = 1;
+                        int lines_count = 1;
+                        StreamReader file =
+                            new StreamReader(getFile);
+                        string line;
+                        while ((line = file.ReadLine()) != null)
+                        {
+                            int line_index = counter - 1;
+                            if (System.Text.RegularExpressions.Regex.IsMatch(line, txtSearchTerm.Text))
+                            {
+
+                                //if (lines_count > 1)
+                                //{
+                                Array.Resize(ref this.SearchResults, counter);
+                                //}
+                                String get_cur_path = getFile.Replace(cur_path, "");
+                                get_cur_path = get_cur_path.Replace("\\", "/");
+                                this.SearchResults[lines_count - 1] = new SearchResult { Path = get_cur_path + ":" + counter, FullPath = getFile, Line = line, OpenNotePad = "View in NotePad" };
+
+                                lines_count++;
+                            }
+                            counter++;
+                        }
+                        file.Close();
+                    }
+                }
+                else
+                {
+                    List<String> foundFiles = new List<String>();
+                    foreach (String getFile in files)
+                    {
+                        int counter = 1;
+                        int lines_count = 1;
+                        StreamReader file =
+                            new StreamReader(getFile);
+                        string line;
+                        while ((line = file.ReadLine()) != null)
+                        {
+                            int line_index = counter - 1;
+                            if (System.Text.RegularExpressions.Regex.IsMatch(line, txtSearchTerm.Text))
+                            {
+                                String get_cur_path = getFile.Replace(cur_path, "");
+                                get_cur_path = get_cur_path.Replace("\\", "/");
+
+                                var match = foundFiles.FirstOrDefault(stringToCheck => stringToCheck.Contains(get_cur_path));
+                                if (match == null)
+                                {
+                                    //if (lines_count > 1)
+                                    //{
+                                    Array.Resize(ref this.SearchResults, counter);
+                                    //}
+
+                                    this.SearchResults[lines_count - 1] = new SearchResult { Path = get_cur_path, FullPath = "", Line = "", OpenNotePad = "" };
+                                    foundFiles.Add(get_cur_path);
+                                    lines_count++;
+                                }
+                            }
+                            counter++;
+                        }
+                        file.Close();
+                    }
+                }
+               
+                dgvSearchResults.DataSource = this.SearchResults;
+            }
+            else
+            {
+                this.SearchResults = new SearchResult[0];
+                dgvSearchResults.DataSource = this.SearchResults;
+            }
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dgvSearchResults_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (cboJustFiles.Checked == false)
+            {
+                Process cmd = new Process();
+                cmd.StartInfo.FileName = "cmd.exe";
+                cmd.StartInfo.RedirectStandardInput = true;
+                cmd.StartInfo.RedirectStandardOutput = true;
+                cmd.StartInfo.CreateNoWindow = true;
+                cmd.StartInfo.UseShellExecute = false;
+                cmd.Start();
+
+                cmd.StandardInput.WriteLine("start notepad++ " + this.SearchResults[e.RowIndex].FullPath);
+                cmd.StandardInput.Flush();
+                cmd.StandardInput.Close();
+                cmd.WaitForExit();
+                Console.WriteLine(cmd.StandardOutput.ReadToEnd());
+            }
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+    public class SearchResult
+    {
+        public String Path { get; set; }
+        public String FullPath { get; set; }
+        public String Line { get; set; }
+        public String OpenNotePad { get; set; }
+    }
+}
